155dea573916127fa4265babb74a89ed8c1a7e9c
Groha <86876291+Groha@users.noreply.github.com>
2021-09-06 22:52:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+let kitchenProducts = [
+	{
+		type: 'grater',
+		price: 10
+	},
+	{
+		type: 'pastry-bag',
+		price: 25
+	},
+	{
+		type: 'scale',
+		price: 5
+	},
+	{
+		type: 'whisk',
+		price: 15
+	}
+];
+
+let devicesProducts = [
+	{
+		type: 'desktop',
+		price: [100,1000]
+	},
+	{
+		type: 'laptop',
+		price: [50,1500]
+	},
+	{
+		type: 'smartphone',
+		price: [80,2000]
+	},
+	{
+		type: 'tablet',
+		price: [20,1300]
+	}
+];
+
+let cosmeticsProducts = [
+	{
+		type: 'blush',
+		price: 100
+	},
+	{
+		type: 'eyeshadow',
+		price: 50
+	},
+	{
+		type: 'lipstick',
+		price: 80
+	},
+	{
+		type: 'nail-polish',
+		price: 200
+	},
+	{
+		type: 'perfume',
+		price: 300,
+	}
+];
+
+const allProdusts = {
+   kitchen: kitchenProducts,
+   devices: devicesProducts,
+   cosmetics: cosmeticsProducts,
+}
+
+let renderTable = ``
+
+function Product(category, type, price) {
+   this.category = category;
+   this.type = type;
+   this.price = price;
+}
+
+Product.prototype.render = function() {
+   return `<tr>
+            <td><img src="images/${this.category}/${this.type}.svg" alt="${this.type}" width="50" height="50"></td>
+            <td>${this.type}</td>
+            <td>${this.price} USD</td>
+         </tr>`;
+}
+
+function someProduct(product, categoryName) {
+   product.forEach (item => {
+      let priceRange = Array.isArray(item.price) ? item.price.join(`-`) : item.price;
+      let category = new Product(`${categoryName}`, `${item.type}`, `${priceRange}`);
+      renderTable += `${category.render()}`; 
+   })
+}
+
+for (let key in allProdusts) {
+   someProduct(allProdusts[key], key);
+}
+
+document.write(`<table border="1px"><tr><th>Image</th><th>Name</th><th>Price</th></tr>${renderTable}</table>`)
